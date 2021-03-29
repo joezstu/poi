33e3b3bf7cfba692b7a285a79cf1cf59c8531d84
joezstu <joezstu@gmail.com>
2021-03-29 17:59:42 +0800

    chore: 配置vue和element
    包括配置一些路由和基础调试页面
    
    foot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,18 @@
-<form action="/export" method="post" enctype="multipart/form-data" >
-    {{csrf_field()}}
-    <input type="file" name="e_file"/>
-    <button>上传</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+</head>
+
+<body>
+<div id="app">
+    <router-view></router-view>
+</div>
+<script src="{{mix('/js/app.js')}}"></script>
+</body>
+
+</html>
