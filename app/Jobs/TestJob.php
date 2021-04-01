b129734c6597c28be3a046259a375938b1b82e29
joezstu <joezstu@gmail.com>
2021-04-01 22:02:19 +0800

    修bug：address

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -59,10 +59,8 @@ class TestJob implements ShouldQueue
                         $string = '';
 
                         foreach($r as $kk => $vv){
-                            if(is_object($vv)){
-                                $string .= $vv->name ."-" . ((property_exists($vv,'address') && is_string($vv->address)) ? $vv->address : '无') . '-' . $vv->distance . '米';
-                                $string .= "\n";
-                            }
+                            $string .= $vv->name ."-" . (isset($vv->address) && is_string($vv->address) ? $vv->address : '无') . '-' . $vv->distance . '米';
+                            $string .= "\n";
                         }
 
                         if(is_string($value->pname) && !empty($value->pname)){
