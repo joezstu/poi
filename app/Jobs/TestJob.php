b14113280c22affbc27a03ba7935c3db00cffef9
joezstu <joezstu@gmail.com>
2021-04-01 21:36:35 +0800

    修bug：api返回的对象没有address属性的情况

--- app/Jobs/TestJob.php
+++ app/Jobs/TestJob.php
@@ -59,7 +59,7 @@ class TestJob implements ShouldQueue
                         $string = '';
 
                         foreach($r as $kk => $vv){
-                            $string .= $vv->name ."-" . (is_string($vv->address) ? $vv->address : '无') . '-' . $vv->distance . '米';
+                            $string .= $vv->name ."-" . (isset($vv->address) && is_string($vv->address) ? $vv->address : '无') . '-' . $vv->distance . '米';
                             $string .= "\n";
                         }
 
